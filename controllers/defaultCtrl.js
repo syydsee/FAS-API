d7a89f01447a069cd76d8839b43f09d856044e78
syydsee <syydsee@gmail.com>
2023-12-22 11:22:43 +0530

    health endpoint

--- controllers/defaultCtrl.js
+++ controllers/defaultCtrl.js
@@ -1,11 +1,30 @@
+const mongoose= require('mongoose');
+const config = require('../config');
+
 const get = (req, res) => {
     res.status(200);
     res.send('FAS API');
 };
 
-const health = (req, res)=>{
-    res.status(201);
-    res.json({status:'Up'});
+const health = async(req, res)=>{
+    try{
+        await mongoose.connect(config.dbConStr)
+        res.status(201);
+        res.json({db:'Up'}); 
+    }catch(e){
+        res.status(500);
+        res.send('Internal Server Error');
+    }
+    // mongoose.connect(config.dbConStr)
+    //     .then(()=>{
+    //         res.status(201);
+    //         res.json({db:'Up'});
+    //     })
+    //     .catch(err =>{
+    //         res.status(500);
+    //         res.send('Internal Server Error');
+    //     });
+    
 }; 
 
 module.exports = {get,health};
--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const defaultRouter = require('./routes/defaultRouter');
+const mongoose = require('mongoose');
+const config = require('./config/index');
 
 const app = express();
 const port = 3000;
@@ -7,5 +9,9 @@ const port = 3000;
 app.listen(port,()=>{
     console.log(`Server is running on port ${port}`);
 });
+
+mongoose.connect(config.dbConStr)
+.then(res=>console.log('Connected to MongoDb'))
+.catch(err=>console.log('failed to connect to db'));
 
 app.use('/', defaultRouter);
