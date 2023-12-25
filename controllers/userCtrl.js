ee08c3e593230846325d2dfce61b25d55e9c4803
syydsee <syydsee@gmail.com>
2023-12-25 21:35:00 +0530

    Validation and erros

--- controllers/userCtrl.js
+++ controllers/userCtrl.js
@@ -1,15 +1,28 @@
 const userRepository = require('../repositories/userRepository'); 
+
+const alreadyExists = (e)=>e.message && e.message.indexOf('dubicate key') > -1;
+
+const hasErrors = (e)=>e._message==='user validation failed';
+
+const handleErrors=(e,res)=>{
+    if (alreadyExists(e)) 
+        res.status(409).send('User already exists'); 
+    else if (hasErrors(e))
+        res.status(400).json(e.errors);
+    else
+        res.status(500).send('Internal Server Error');
+}
+
 const register = async(req,res) => {
     try{
         const data = req.body;
+        data.createdAt = Date.now;
         console.log(data, 'body');
         await userRepository.add(data);
         res.status(201);
         res.send();
-    }catch(e) {
-        console.log(e);
-        res.status(500);
-        res.send('Instenal Server Error');
+    }catch (e) {
+        handleErrors(e, res);
     }
 };
 
--- models/userModel.js
+++ models/userModel.js
@@ -2,21 +2,38 @@ const mongoose= require('mongoose');
 
 
 const userModel = mongoose.model('user',{
-    firstName: String,
-    lastName: String,
+    firstName: {
+        type: String, 
+        minLenght: [3, 'Min 3 Charaters'],
+        maxLength: [100, 'Max 100 Charaters'],
+        required: [true, 'First name is mandatory']},
+    lastName: {
+        type: String, 
+        required: [true, 'Last name is required']},
+    email: {
+        type: String,
+        unique: true, 
+        required: [true, 'Email is mandatory']},
+    password: {
+        type: String, 
+        required: [true, 'Password is mandatory']},
     gender: String,
-    password: String,
-    active: Boolean,
+    active: {type: Boolean, default: 1},
     gender: Number,
-    email: String,
-    mobile: String,
+    mobile: {
+        type: String,
+        validate: {
+            validator: v => /[0-9]{10}/.test(v),
+            message: () => 'Invalid phone number'
+        }
+    },
     qualification: Number,
     degree: Number,
     image: String,
     skills: [String],
     passout: Number,
     createdAt: Date,
-    updatedAt: Date
+    updatedAt: {type:Date, default: Date.now }
 
 });
 
